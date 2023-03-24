85e8eaae41b93d0cc76fd993fd75f667d16d98ca
libin ann babu <libin.babu@altd.in>
2023-03-24 17:02:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.tutussfunny.com/laravel-9-vue-js-3-crud-application/
---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employees = Employee::all();
+        return response()->json($employees);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $employee = new Employee([
+            'name' => $request->input('name'),
+            'address' => $request->input('address'),
+            'mobile' => $request->input('mobile')
+        ]);
+        $employee->save();
+        return response()->json('Employee created!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $employee = Employee::find($id);
+        return response()->json($employee);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $employees = Employee::find($id);
+        $employees->update($request->all());
+        return response()->json('Employee updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $employee = Employee::find($id);
+        $employee->delete();
+        return response()->json('Employee deleted');
+    }
+}
